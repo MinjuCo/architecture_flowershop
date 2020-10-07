3e3bc1744c0c84b87a36aec506c3742cd1811a8a
Madina Jurakholova <madina.jurakholova@gmail.com>
2020-10-07 13:39:58 +0200

    Dotnet project creation with mvc

---
+++ BasicRestApi.API/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BasicRestApi.API.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
